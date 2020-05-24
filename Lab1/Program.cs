1371ce4421d87ce3acc460ff3633cb423d9e6263
Hymeck <noonimf@gmail.com>
2020-05-24 23:28:08 +0300

    initial commit

---
+++ Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Pdf;
+using OxyPlot.Axes;
+
+namespace Lab1
+{
+    class Program
+    {
+
+        // Y = cos(x); a = -pi/2; b = pi/2; y_0 = 0.5;
+        // f(y) - ?; write f(y_0)
+        // http://www.alexeypetrov.narod.ru/C/r_gener_about.html
+        // https://habr.com/ru/post/263993/
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
