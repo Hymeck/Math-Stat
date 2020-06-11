97b6bf3277c3cb21558dd4c17c7d53989c5a4147
Hymeck <noonimf@gmail.com>
2020-06-11 16:23:01 +0300

    lab 4 task2

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -15,10 +15,8 @@ namespace Labs
         static void Main(string[] args)
         {
             //Lab1();
-            //Lab3ChiSquared();
-            //Lab3Kolmogorov();
-            //Lab3Mises();
-            Lab4Task1();
+            //Lab3();
+            Lab4();
         }
 
         static void Lab1()
@@ -45,6 +43,7 @@ namespace Labs
             chartHandler.Plot("SampleChart.png");
         }
 
+        #region Lab 3
         static void WriteResult(string statisticName, double statistic, bool isConfirmed)
         {
             Console.WriteLine(string.Format("{0} statistic: {1}", statisticName, Math.Round(statistic, 3)));
@@ -106,6 +105,16 @@ namespace Labs
             WriteResult("Mises", misesCriterion.MisesStatistic, misesCriterion.IsConfirmed);
         }
 
+        static void Lab3()
+        {
+            Lab3ChiSquared();
+            Lab3Kolmogorov();
+            Lab3Mises();
+        }
+
+        #endregion Lab 3
+
+        #region Lab 4
         // gammas: 0.90, 0.95, 0.98, 0.99
         static double[] gammas = new double[] { 0.90, 0.95, 0.98, 0.99 };
 
@@ -120,6 +129,27 @@ namespace Labs
             {150, new double[] { 1.6551445337952997, 1.976013177679155, 2.3516348950235146, 2.6092279073321927 } },
         };
 
+        static Dictionary<int, double[]> chiSquaredLeftValues = new Dictionary<int, double[]>
+        {
+            {20, new double[] { 31.410432844230918, 34.16960690283833, 37.56623478662507, 39.99684631293865 } },
+            {30, new double[] { 43.77297182574219, 46.97924224367115, 50.89218131151707, 53.671961930240585 } },
+            {50, new double[] { 67.5048065495412, 71.42019518750642, 76.1538912490127, 79.48997846682893 } },
+            {70, new double[] { 90.53122543488065, 95.02318419040617, 100.42518422881135, 104.21489877981679 } },
+            {100, new double[] { 124.34211340400407, 129.5611971858366, 135.80672317102676, 140.1694894423138 } },
+            {150, new double[] { 179.58063415418053, 185.80044700379327, 193.20768638551056, 198.36020599864545 } },
+        };
+
+        static Dictionary<int, double[]> chiSquaredRightValues = new Dictionary<int, double[]>
+        {
+            {20, new double[] { 10.850811394182585, 9.590777392264867, 8.260398332546398, 7.433844262934232 } },
+            {30, new double[] { 18.49266098195347, 16.790772265566623, 14.953456528455447, 13.786719859502718 } },
+            {50, new double[] { 34.76425168350175, 32.357363695658655, 29.70668269884127, 27.99074886637332 } },
+            {70, new double[] { 51.739278048962916, 48.757564805039536, 45.44171731081055, 43.27517954582346 } },
+            {100, new double[] { 77.92946516501726, 74.22192747492373, 70.0648949253998, 67.32756330547917 } },
+            {150, new double[] { 122.69177538711729, 117.98451540290291, 112.66758283241268, 109.14224810953266 } },
+        };
+
+        // n
         static int[] volumes = new int[] { 20, 30, 50, 70, 100, 150 };
 
         // D(X) = (b - a)^2 / 12;
@@ -135,30 +165,42 @@ namespace Labs
         }
 
         // S * t_gamma_n-1 / sqrt(n - 1)
-        static double Deviation(double sampleVariance, int volume, double studentTFunctionValue)
+        static double MeanDeviation(double variance, int volume, double studentTFunctionValue)
         {
-            return sampleVariance * studentTFunctionValue / Math.Sqrt(volume - 1);
+            return studentTFunctionValue * Math.Sqrt(variance / volume - 1);
         }
 
-        static void Draw(double[] deviations, string fileName)
+        // n * S^2 / chi-squared
+        static double VarianceDeviation(double varince, int volume, double chiSquared)
+        {
+            return volume * varince / chiSquared;
+        }
+
+        static void Draw(double[] deviations, double[] deviationsWithTrueCharacteristic, string fileName, bool isMean = true)
         {
             var plt = new Plot();
 
-            plt.PlotScatter(gammas, deviations.Select(x => x * 2).ToArray());
+            if (isMean)
+            {
+                plt.PlotScatter(gammas, deviations.Select(x => x * 2).ToArray(), label: "With unknown variance");
+                plt.PlotScatter(gammas, deviationsWithTrueCharacteristic.Select(x => x * 2).ToArray(), label: "With known variance");
+            }
+
+            else
+            {
+                plt.PlotScatter(gammas, deviations, label: "With unknown mean");
+                plt.PlotScatter(gammas, deviationsWithTrueCharacteristic, label: "With known mean");
+            }
+
             plt.YLabel("Confidence interval value");
             plt.XLabel("Significance level");
+            plt.Legend();
 
             plt.SaveFig(fileName);
         }
 
-        static void Lab4Task1()
+        static void Lab4Task1(double analyticalVariance)
         {
-            double mean = AnalyticalMean(0, 1),
-                   variance = AnalyticalVariance(0, 1);
-
-            Console.WriteLine($"Analytical mean: {mean}");
-            Console.WriteLine($"Analytical variance: {variance}\n");
-
             int count = 1;
             foreach (int volume in volumes)
             {
@@ -169,7 +211,10 @@ namespace Labs
                        unbiasedSampleVariance = sampleHandler.UnbiasedSampleVariance;
 
                 double[] deviations = studentTFunctionValues[volume].Select
-                                                                    (x => Deviation(unbiasedSampleVariance, volume, x))
+                                                                    (x => MeanDeviation(unbiasedSampleVariance, volume, x))
+                                                                    .ToArray();
+                double[] deviationsWithTrueVariance = studentTFunctionValues[volume].Select
+                                                                    (x => MeanDeviation(analyticalVariance, volume, x))
                                                                     .ToArray();
 
                 Console.WriteLine($"Volume: {volume}");
@@ -178,15 +223,66 @@ namespace Labs
 
                 for (int i = 0; i < deviations.Length; i++)
                 {
-                    Console.WriteLine($"{Math.Round(sampleMean - deviations[i], 4)} <= m_x <= " +
+                    Console.WriteLine($"{Math.Round(sampleMean - deviations[i], 4)} <= Mean <= " +
                                       $"{Math.Round(sampleMean + deviations[i], 4)} " +
                                       $"Student t-function value = {Math.Round(studentTFunctionValues[volume][i], 4)}.");
                 }
                 Console.WriteLine();
 
-                Draw(deviations, $"Lab3Task1_Gamma-dependence_{count}.png");
+                Draw(deviations, deviationsWithTrueVariance, $"Lab3Task1_Gamma-dependence_{count}.png");
                 count++;
             }
         }
+
+        static void Lab4Task2()
+        {
+            int count = 1;
+            foreach (int volume in volumes)
+            {
+                double[] arguments = new VariableGenerator(leftBound, rightBound, (uint)volume).GetVariables();
+                var sampleHandler = new SampleHandler(arguments, x => Math.Cos(x), 9);
+
+                double sampleMean = sampleHandler.SampleMean,
+                       unbiasedSampleVariance = sampleHandler.UnbiasedSampleVariance,
+                       biasedSampleMean = sampleHandler.BiasedSampleVariance;
+
+                double[] leftDeviations = chiSquaredLeftValues[volume].Select(x => VarianceDeviation(unbiasedSampleVariance, volume, x)).ToArray();
+                double[] rightDeviations = chiSquaredRightValues[volume].Select(x => VarianceDeviation(unbiasedSampleVariance, volume, x)).ToArray();
+
+
+                double[] leftDeviationsWithBiasedVariance = chiSquaredLeftValues[volume].Select(x => VarianceDeviation(biasedSampleMean, volume, x)).ToArray();
+                double[] rightDeviationsWithBiasedVariance = chiSquaredRightValues[volume].Select(x => VarianceDeviation(biasedSampleMean, volume, x)).ToArray();
+
+                Console.WriteLine($"Volume: {volume}");
+                Console.WriteLine($"Sample mean: {Math.Round(sampleMean, 4)}");
+                Console.WriteLine($"Unbiased sample variance: {Math.Round(unbiasedSampleVariance, 4)}");
+
+                double[] deviations = new double[leftDeviations.Length];
+                double[] deviationsWithTrueMean = new double[leftDeviations.Length];
+                for (int i = 0; i < leftDeviations.Length; i++)
+                {
+                    deviationsWithTrueMean[i] = rightDeviationsWithBiasedVariance[i] - leftDeviationsWithBiasedVariance[i];
+                    deviations[i] = rightDeviations[i] - leftDeviations[i];
+
+                    Console.WriteLine($"{Math.Round(unbiasedSampleVariance - leftDeviations[i], 4)} <= Variance <= " +
+                                      $"{Math.Round(unbiasedSampleVariance + rightDeviations[i], 4)} " +
+                                      $"Chi-squared left = {Math.Round(chiSquaredLeftValues[volume][i], 4)}, " +
+                                      $"Chi-squared right = {Math.Round(chiSquaredRightValues[volume][i], 4)}");
+                }
+                Draw(deviations, deviationsWithTrueMean, $"Lab3Task2_Gamma-dependence_{count}.png", false);
+                count++;
+            }
+        }
+
+        static void Lab4()
+        {
+            double analyticalVariance = AnalyticalVariance(0, 1);
+            Console.WriteLine($"Analytical mean: {AnalyticalMean(0, 1)}");
+            Console.WriteLine($"Analytical variance: {analyticalVariance}\n\n");
+            Lab4Task1(analyticalVariance);
+            Console.WriteLine("\n\n\n");
+            Lab4Task2();
+        }
+        #endregion Lab 4
     }
 }
